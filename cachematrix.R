f1f22bd922ce758e098d68e2b7230758f65da19d
Riddhi203 <72273119+Riddhi203@users.noreply.github.com>
2020-10-02 22:45:06 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -2,11 +2,25 @@
 ## functions do
 
 ## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+ m <<- NULL
+        }
+        get <- function() x
+        setmean <- function(mean) m <<- mean
+        getmean <- function() m
+        list(set = set, get = get,
+             setmean = setmean,
+             getmean = getmean)
 }
-
+makeCacheMatrix <- function(x = matrix()) {
+ m <- x$getmean()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+}
+data <- x$get()
+        m <- mean(data, ...)
+        x$setmean(m)
+        m
 
 ## Write a short comment describing this function
 
